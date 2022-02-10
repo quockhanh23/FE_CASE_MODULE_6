fae6d887b7340130257d8d2ca9e6a87380ded3c9
uyle1234 <linhconuong2k3@gmail.com>
2022-02-10 11:16:40 +0700

    itf recruitments

--- src/app/models/recruitments.ts
+++ src/app/models/recruitments.ts
@@ -1,2 +1,12 @@
 export interface Recruitments {
+   id?:String;
+   title?:String;
+   salary?:String;
+   address?:String;
+   description?:String;
+   experience?:String;
+   numberOfRecruitments?:String;
+   dateEnd?:String;
+   gender?:String;
 }
+
