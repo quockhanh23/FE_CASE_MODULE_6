b64f7b8bbe07325d747f350b3d34f41d02911e5c
hoangducviet1993 <ducviet12vl@gmail.com>
2022-02-10 20:51:48 +0700

    tạo details -job

---
+++ src/app/main/enterprise/recruitments/recruitments-details/recruitments-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recruitments-details',
+  templateUrl: './recruitments-details.component.html',
+  styleUrls: ['./recruitments-details.component.css']
+})
+export class RecruitmentsDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
