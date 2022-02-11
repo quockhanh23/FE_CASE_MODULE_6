bcfcce696f038bda055208c56dcdb117bd53501d
hoangducviet1993 <ducviet12vl@gmail.com>
2022-02-11 13:15:25 +0700

    fix css

---
+++ src/app/main/enterprise/home-page-enterprise/home-page-enterprise.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page-enterprise',
+  templateUrl: './home-page-enterprise.component.html',
+  styleUrls: ['./home-page-enterprise.component.css']
+})
+export class HomePageEnterpriseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
