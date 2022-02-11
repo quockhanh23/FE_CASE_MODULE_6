3b4c28fb307a9c9b90f377013dc766e9630168a5
unknown <yeuthuongvoivangdiqua@gmail.com>
2022-02-11 16:48:48 +0700

    mang ve nha lam

--- src/app/main/admin/list-enterprise-of-admin/list-enterprise-of-admin.component.ts
+++ src/app/main/admin/list-enterprise-of-admin/list-enterprise-of-admin.component.ts
@@ -1,24 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ProfileEnterprise} from "../../../models/profile-enterprise";
 import {ProfileEnterpriseOfAdminService} from "../../../services/profile-enterprise-of-admin.service";
 import {MatDialog} from "@angular/material/dialog";
 import {ToastrService} from "ngx-toastr";
 import {Router} from "@angular/router";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {DialogSuccessComponent} from "../../../notification/dialog-success/dialog-success.component";
+import {DialogFailComponent} from "../../../notification/dialog-fail/dialog-fail.component";
 
 
 @Component({
   selector: 'app-list-enterprise-of-admin',
   templateUrl: './list-enterprise-of-admin.component.html',
-  styleUrls: ['./list-enterprise-of-admin.component.css']
+  styleUrls: ['./list-enterprise-of-admin.component.css'],
+
 })
 export class ListEnterpriseOfAdminComponent implements OnInit {
-
+  closeResult!: string;
   enterprise!: ProfileEnterprise[]
 
   constructor(private enterpriseOfAdminService: ProfileEnterpriseOfAdminService,
               public dialog: MatDialog,
               private toarts: ToastrService,
               private router: Router,
+
   ) {
   }
 
@@ -35,30 +40,44 @@ export class ListEnterpriseOfAdminComponent implements OnInit {
   changeStatusLock(id: any) {
     this.enterpriseOfAdminService.changeStatusLock(id).subscribe(result => {
       console.log(result)
-      this.router.navigateByUrl("/admin/list")
+      this.router.navigateByUrl("/admin/list").then()
       this.ngOnInit()
+      this.openDialogSuccess()
     }, error => {
       console.log("Lỗi", error)
+      this.openDialogFail()
     })
   }
 
   changeStatusActive(id: any) {
     this.enterpriseOfAdminService.changeStatusActive(id).subscribe(result => {
       console.log(result)
-      this.router.navigateByUrl("/admin/list")
+      this.router.navigateByUrl("/admin/list").then()
       this.ngOnInit()
+      this.openDialogSuccess()
     }, error => {
       console.log("Lỗi", error)
+      this.openDialogFail()
     })
   }
 
   openToartSuccess() {
     this.toarts.success('mes', 'title')
   }
+
   openToartError() {
     this.toarts.error('mes', 'title')
   }
+
   openToartInFor() {
     this.toarts.info('mes', 'title')
+  }
+
+  openDialogSuccess() {
+    this.dialog.open(DialogSuccessComponent);
+  }
+
+  openDialogFail() {
+    this.dialog.open(DialogFailComponent);
   }
 }
